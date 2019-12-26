dc39f9160bf9c08edf9f662fc57f3b1cb9acf9a9
Enaho Murphy <enahomurphy@gmail.com>
2019-12-26 18:09:10 +0100

    initial commit

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const lessToJS = require('less-vars-to-js')
+const fs = require('fs')
+
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, '../assets/antd-custom.less'), 'utf8')
+)
+
+module.exports = ({ config, mode }) => {
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [['react-app', { flow: false, typescript: true }]],
+      }
+    },
+    {
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../') 
+    },
+    {
+      test: /\.less$/,
+      use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'less-loader', options: {
+              modifyVars: themeVariables,
+              javascriptEnabled: true
+            }
+          },
+      ],
+      include: /node_modules/,
+    },
+  );
+
+  return config;
+};
